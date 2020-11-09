9af8e7d3269df9a56c7b5a5557241039b3775e84
Enrico Fasoli <enrico.fasoli1996@gmail.com>
2020-11-10 00:00:47 +0100

    camera

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,34 +1,60 @@
-use rustbox::Key;
+use rustbox::{Key, RustBox};
 
 pub mod render;
 
 pub struct Entity {
-    pub x: usize,
-    pub y: usize,
+    pub x: i32,
+    pub y: i32,
 }
 
 pub struct Game {
     pub player: Entity,
+    rustbox: RustBox,
 }
 
-pub fn process_input(key: &Key, game: &mut Game) -> bool {
-    match key {
-        Key::Char('q') => {
-            return false;
-        }
-        Key::Left => {
-            game.player.x -= 1;
-        }
-        Key::Right => {
-            game.player.x += 1;
-        }
-        Key::Up => {
-            game.player.y -= 1;
-        }
-        Key::Down => {
-            game.player.y += 1;
-        }
-        _ => {}
+impl Game {
+    pub fn new() -> Game {
+        let rustbox = match RustBox::init(Default::default()) {
+            Result::Ok(v) => v,
+            Result::Err(e) => panic!("Failed: {}", e),
+        };
+        let player = Entity { x: 0, y: 0 };
+        return Game { player: player, rustbox: rustbox };
     }
-    return true;
+
+    pub fn process_input(&mut self, key: &Key) -> bool {
+        match key {
+            Key::Char('q') => {
+                return false;
+            }
+            Key::Left => {
+                self.player.x -= 1;
+            }
+            Key::Right => {
+                self.player.x += 1;
+            }
+            Key::Up => {
+                self.player.y -= 1;
+            }
+            Key::Down => {
+                self.player.y += 1;
+            }
+            _ => {}
+        }
+        return true;
+    }
+
+    pub fn run(&mut self) {
+        loop {
+            render::render(&self.rustbox, &self);
+            match self.rustbox.poll_event(false) {
+                Ok(rustbox::Event::KeyEvent(key)) => {
+                    let continue_game = self.process_input(&key);
+                    if !continue_game { break; }
+                },
+                Err(e) => panic!("{}", e.to_string()),
+                _ => {}
+            }
+        }
+    }
 }
--- src/game/render.rs
+++ src/game/render.rs
@@ -1,5 +1,8 @@
 use rustbox::{Color, RustBox};
 
+const WIDTH: i32 = 80;
+const HEIGHT: i32 = 24;
+
 pub fn render(rustbox: &RustBox, game: &super::Game) {
     rustbox.clear();
     /*
@@ -20,15 +23,34 @@ pub fn render(rustbox: &RustBox, game: &super::Game) {
         "Press 'q' to quit.",
     );
     */
-    render_tree(rustbox, 10, 10);
+    render_tree(rustbox, &game.player, (10, 10));
     render_player(rustbox, &game.player);
     rustbox.present();
 }
 
-fn render_tree(rustbox: &RustBox, x: usize, y: usize) {
-    rustbox.print(x, y, rustbox::RB_NORMAL, Color::Green, Color::Default, "T");
+fn game_coords_to_camera(rustbox: &RustBox, player: &super::Entity, x: i32, y: i32) -> (usize, usize) {
+    let result_x = x + (rustbox.width() as i32) / 2 - player.x;
+    let result_y = y + (rustbox.height() as i32) / 2 - player.y;
+    if result_x < 0 || result_y < 0 {
+        return (rustbox.width()+1, rustbox.height()+1)
+    }
+    (result_x as usize, result_y as usize)
+}
+
+fn is_visible(rustbox: &RustBox, x: usize, y: usize) -> bool {
+    x < rustbox.width() && y < rustbox.height()
+}
+
+fn render_tree(rustbox: &RustBox, player: &super::Entity, coords: (i32, i32)) {
+    let (x, y) = game_coords_to_camera(rustbox, player, coords.0, coords.1);
+    if is_visible(rustbox, x, y) {
+        rustbox.print(x, y, rustbox::RB_NORMAL, Color::Green, Color::Default, "T");
+    }
 }
 
 fn render_player(rustbox: &RustBox, player: &super::Entity) {
-    rustbox.print(player.x, player.y, rustbox::RB_NORMAL, Color::White, Color::Default, "@");
+    let (x, y) = game_coords_to_camera(rustbox, player, player.x, player. y);
+    if is_visible(rustbox, x, y) {
+        rustbox.print(x, y, rustbox::RB_NORMAL, Color::White, Color::Default, "@");
+    }
 }
